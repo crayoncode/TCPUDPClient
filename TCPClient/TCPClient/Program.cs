be943af8190044d18551fd66324466eae00cfc41
ZhaoYang <492118114@qq.com>
2015-12-18 15:19:49 +0800

    客户端

---
+++ TCPClient/TCPClient/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] data = new byte[1024];
+            string input, stringData;
+            
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ipEnd = new IPEndPoint(ip, 5566);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                socket.Connect(ipEnd);
+            }
+            catch (SocketException e)
+            {
+                Console.Write("Fail to connect server");
+                Console.Write(e.ToString());
+                return;
+            }
+            int recv = socket.Receive(data);
+            stringData = Encoding.ASCII.GetString(data, 0, recv);
+            Console.Write(stringData);
+            while (true)
+            {
+                input = Console.ReadLine();
+                if (input == "exit")
+                {
+                    break;
+                }
+                socket.Send(Encoding.ASCII.GetBytes(input));
+                data = new byte[1024];
+
+                recv = socket.Receive(data);
+                stringData = Encoding.ASCII.GetString(data, 0, recv);
+                Console.Write(stringData);
+            }
+            Console.Write("disconnect from server");
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+        }
+    }
+}
---
+++ UDPClient/UDPClient/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          SendBroadcast("hello");
+           Console.ReadKey();
+        }
+
+        public static string SendBroadcast(string msgStr)
+        {
+            try
+            {
+                //New一个Socket配置连接方式
+                using (Socket socks = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
+                {
+                    int recv;
+                    byte[] recvBytes = new byte[1024];
+                    string ipValue = "127.0.0.1";
+                    //获取广播需要的端口
+                    string port = "1514";
+                    //创建连接
+                    IPEndPoint iep = new IPEndPoint(IPAddress.Parse(ipValue), int.Parse(port));
+                    EndPoint remote = (EndPoint)(iep);
+                    //将字符转化为可传输的格式
+                    byte[] data = System.Text.Encoding.UTF8.GetBytes(msgStr);
+                    socks.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 1);
+                    socks.SendTo(data, iep);
+                    recv = socks.ReceiveFrom(recvBytes, ref remote);
+                    string str = System.Text.Encoding.ASCII.GetString(recvBytes, 0, recv);
+                    Console.WriteLine("444:{0}", str);
+                    socks.Dispose();
+                    return "CMD Send Succeed!";
+                }               
+            }
+            catch { return "CMD Send Fail!"; }
+        }
+    }
+}
